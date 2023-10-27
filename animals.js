ff9d432c7bbe2ea2116057f215f8168889cbe461
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 08:34:07 -0300

    cp finalizado

---
+++ animals.js
@@ -0,0 +1,7 @@
+ const animals = ["macaco", "tucano", "elefante", "pavão", "hipopótamo", "leão"];
+
+ if (!animals.includes("leão")){
+    console.log("Leão não existe no array")
+ }else{
+    console.log("Existe leão no array")
+ }
---
+++ series.js
@@ -0,0 +1,2 @@
+const my3FavoriteTVShows = ["Ted Lasso", "B-99", "Foundation"];
+console.log(my3FavoriteTVShows.length === 3);
